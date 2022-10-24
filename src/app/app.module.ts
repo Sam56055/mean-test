46a59f629ea50d8c8fe7d219231ab9005627bfc1
Sam Kharel <kharels@bellsouth.net>
2022-10-23 21:15:08 -0400

    mean-test full

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NameEditorComponent } from './name-editor/name-editor.component';
+import { WarningMessageComponent} from './warning-message/warning-message.component';
+import { SuccessMessageComponent  } from './success-message/success-message.component';
+import { SelectorComponent } from './selector/selector.component';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    WarningMessageComponent,
+    NameEditorComponent, 
+    SuccessMessageComponent, 
+    SelectorComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    ReactiveFormsModule, 
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
